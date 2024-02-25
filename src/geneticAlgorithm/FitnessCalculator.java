3b453feffe6fd5e039b538a26eb5443b8a2f4e9a
GavDotZip <gavdotzip@gmail.com>
2024-02-25 23:45:05 +0000

    comments

--- src/geneticAlgorithm/FitnessCalculator.java
+++ src/geneticAlgorithm/FitnessCalculator.java
@@ -1,4 +1,42 @@
 package geneticAlgorithm;
 
 public class FitnessCalculator {
+    static byte[] solution = new byte[64];
+
+    // Set candidate solution as a byte array
+    public static void setSolution(byte[] newSolution) {
+        solution = newSolution;
+    }
+
+    // Set candidate with string of 0s and 1s
+    static void setSolution(String newSolution) {
+        solution = new byte[newSolution.length()];
+        // Loop to save characters in our byte array
+        for (int i = 0; i < newSolution.length(); i++) {
+            String character = newSolution.substring(i, i + 1);
+            if (character.contains("0") || character.contains("1")) {
+                solution[i] = Byte.parseByte(character);
+            } else {
+                solution[i] = 0;
+            }
+        }
+    }
+
+    // Calculate individual's fitness by comparing to candidate solution
+    static int getFitness(Individual individual) {
+        int fitness = 0;
+        // Loop and compare genes to candidate solution
+        for (int i = 0; i < individual.size() && i < solution.length; i++) {
+            if (individual.getGene(i) == solution[i]) {
+                fitness++;
+            }
+        }
+        return fitness;
+    }
+
+    // Get an optimum fitness
+    static int getMaxFitness() {
+        int maxFitness = solution.length;
+        return maxFitness;
+    }
 }
