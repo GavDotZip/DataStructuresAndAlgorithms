dd4081d0497b84985717b34751639ddea3446877
GavDotZip <gavdotzip@gmail.com>
2024-02-25 23:35:47 +0000

    comments

--- src/geneticAlgorithm/Individual.java
+++ src/geneticAlgorithm/Individual.java
@@ -1,4 +1,51 @@
 package geneticAlgorithm;
 
 public class Individual {
+    static int defaultGeneLength = 64;
+    private byte[] genes = new byte[defaultGeneLength];
+    private int fitness = 0;
+
+    // Creates a random individual
+    public void generateIndividual() {
+        for (int i = 0; i < size(); i++) {
+            byte gene = (byte) Math.round(Math.random());
+            genes[i] = gene;
+        }
+    }
+
+    // Setters and getters
+    // Use this if you want to create individuals with different gene lengths
+    public static void setDefaultGeneLength(int length) {
+        defaultGeneLength = length;
+    }
+
+    public byte getGene(int index) {
+        return genes[index];
+    }
+
+    public void setGene(int index, byte value) {
+        genes[index] = value;
+        fitness = 0; // Reset fitness since gene has changed
+    }
+
+    /* Public methods */
+    public int size() {
+        return genes.length;
+    }
+
+    public int getFitness() {
+        if (fitness == 0) {
+            fitness = FitnessCalculator.getFitness(this);
+        }
+        return fitness;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder geneString = new StringBuilder();
+        for (int i = 0; i < size(); i++) {
+            geneString.append(getGene(i));
+        }
+        return geneString.toString();
+    }
 }
